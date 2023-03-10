2e195a75ef122029ca44c791e8f8c3fd3134166a
marouaneXan <marouanezahaoui835@gmail.com>
2023-03-10 20:16:50 +0100

    generate sign_in component

--- src/app/features/auth/auth.module.ts
+++ src/app/features/auth/auth.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
+import { SignInComponent } from './components/sign-in/sign-in.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SignInComponent
+  ],
   imports: [
     CommonModule,
     AuthRoutingModule
