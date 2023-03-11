0757712fc448b7b9c160517e20ede7eb9650df33
marouaneXan <marouanezahaoui835@gmail.com>
2023-03-11 19:38:07 +0100

    show and hide a loading

--- src/app/core/services/loading.service.ts
+++ src/app/core/services/loading.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoadingService {
+  private isLoadingSubject=new BehaviorSubject<boolean>(false)
+  public isLoading=this.isLoadingSubject.asObservable()
 
-  constructor() { }
+  show(){
+    this.isLoadingSubject.next(true)
+  }
+  hide(){
+    this.isLoadingSubject.next(false)
+  }
 }
