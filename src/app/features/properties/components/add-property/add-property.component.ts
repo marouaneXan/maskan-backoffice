87f1e7caaae6b0423a459412049cfe081713eb66
marouaneXan <marouanezahaoui835@gmail.com>
2023-03-14 20:42:47 +0100

    generate add property component

---
+++ src/app/features/properties/components/add-property/add-property.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-property',
+  templateUrl: './add-property.component.html',
+  styleUrls: ['./add-property.component.css']
+})
+export class AddPropertyComponent {
+
+}
--- src/app/features/properties/properties.module.ts
+++ src/app/features/properties/properties.module.ts
@@ -1,12 +1,14 @@
 import { NgModule,CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ListPropertiesComponent } from './components/list-properties/list-properties.component';
+import { AddPropertyComponent } from './components/add-property/add-property.component';
 
 
 
 @NgModule({
   declarations: [
-    ListPropertiesComponent
+    ListPropertiesComponent,
+    AddPropertyComponent
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   imports: [
