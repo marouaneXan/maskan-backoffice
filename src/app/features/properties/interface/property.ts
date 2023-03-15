78c87822b71d636c104a186fbb16d6e1545703ba
marouaneXan <marouanezahaoui835@gmail.com>
2023-03-15 22:06:27 +0100

    add interface for property

---
+++ src/app/features/properties/interface/property.ts
@@ -0,0 +1,26 @@
+export interface Category {
+    _id: string,
+    type: string
+}
+export interface Type {
+    _id: string,
+    type: string
+}
+export interface Characteristic {
+    _id: string,
+    name: string
+}
+export interface Property {
+    _id: string
+    title: string,
+    adress: string,
+    bedrooms: string,
+    city: string,
+    stage: string,
+    price: string,
+    size: string,
+    desc: string,
+    category: Category,
+    type: Type,
+    characteristic: Characteristic,
+}
