5c8c369494c7d283852dab3a3f0ea3d92c761639
marouaneXan <marouanezahaoui835@gmail.com>
2023-03-15 14:28:39 +0100

    get current url route and printed in navbar

--- src/app/views/partials/navbar/navbar.component.ts
+++ src/app/views/partials/navbar/navbar.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router,NavigationStart } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
-
+export class NavbarComponent implements OnInit {
+  currentRoute: string | null= sessionStorage.getItem('currentRoute');
+  constructor(private router:Router){}
+  ngOnInit() {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationStart) {
+        this.currentRoute = event.url ;
+        sessionStorage.setItem('currentRoute', this.currentRoute);
+      }
+    });
+  }
 }
