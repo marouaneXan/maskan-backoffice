ad20d845870621aee30062d9cac68cd26b75f1f4
marouaneXan <marouanezahaoui835@gmail.com>
2023-03-18 16:05:51 +0100

    generate characteristic component

---
+++ src/app/shared/characteristic-select/characteristic-select.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CharacteristicSelectComponent } from './characteristic-select/characteristic-select.component';
+
+
+
+@NgModule({
+  declarations: [
+    CharacteristicSelectComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CharacteristicSelectModule { }
---
+++ src/app/shared/characteristic-select/characteristic-select/characteristic-select.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-characteristic-select',
+  templateUrl: './characteristic-select.component.html',
+  styleUrls: ['./characteristic-select.component.css']
+})
+export class CharacteristicSelectComponent {
+
+}
