ad99918747170fb8e7ed4b3c72f03158a09337bd
marouaneXan <marouanezahaoui835@gmail.com>
2023-03-18 15:34:51 +0100

    generate module and component for categories-select

---
+++ src/app/shared/categories-select/categories-select.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CategoriesSelectComponent } from './categories-select/categories-select.component';
+
+
+
+@NgModule({
+  declarations: [
+    CategoriesSelectComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CategoriesSelectModule { }
