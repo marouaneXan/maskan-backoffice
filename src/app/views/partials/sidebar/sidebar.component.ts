a653c5690b12324c1e4b9e911699c184390cd604
marouaneXan <marouanezahaoui835@gmail.com>
2023-03-24 14:53:20 +0000

    add some link and icons + logout functionality

--- src/app/views/partials/sidebar/sidebar.component.ts
+++ src/app/views/partials/sidebar/sidebar.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { TokenService } from 'src/app/core/services/token.service';
 
 @Component({
   selector: 'app-sidebar',
@@ -7,19 +9,39 @@ import { Component } from '@angular/core';
 })
 export class SidebarComponent {
   collapseShow = "hidden";
+  logoSrc:string='../../../../assets/images/maskan-logo.svg'
+  constructor(private tokenService:TokenService,private router:Router){}
   sidebarLinks = [
     {
       path: '/admin/dashboard',
-      name: 'Dashboard'
+      name: 'Dashboard',
+      icon:'fa-solid fa-gauge'
     },
     {
       path: '/admin/dashboard/clients',
-      name: 'Clients'
+      name: 'Clients',
+      icon:'fa-solid fa-users'
     },
     {
       path: '/admin/dashboard/properties',
-      name: 'Properties'
-    }
+      name: 'Properties',
+      icon:'fa-solid fa-house-signal'
+    },
+    {
+      path: '/admin/dashboard/categories',
+      name: 'Categories',
+      icon:'fa-solid fa-layer-group'
+    },
+    {
+      path: '/admin/dashboard/types',
+      name: 'Types',
+      icon:'fa-solid fa-bolt'
+    },
+    {
+      path: '/admin/dashboard/characteristics',
+      name: 'Characteristics',
+      icon:'fa-solid fa-chart-simple'
+    },
   ]
   activeLinkIndex: number = 0;
 
@@ -28,5 +50,12 @@ export class SidebarComponent {
   }
   toggleCollapseShow(classes: string) {
     this.collapseShow = classes;
+  }
+  //logout
+  logout(){
+    this.tokenService.clearLocalStorage()
+    setTimeout(()=>{
+      this.router.navigate(['/auth/sign-in'])
+    },1000)
   }
 }
