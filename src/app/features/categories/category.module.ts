f7bf7277523e2b52427b381c0e3d40feb7eb48b3
marouaneXan <marouanezahaoui835@gmail.com>
2023-03-25 03:27:42 +0000

    ge,erate add, delete and update category components

--- src/app/features/categories/category.module.ts
+++ src/app/features/categories/category.module.ts
@@ -4,12 +4,18 @@ import { HttpClientModule } from '@angular/common/http';
 import { ListCategoriesComponent } from './components/list-categories/list-categories.component';
 import { SpinnerModule } from '../spinner/spinner.module';
 import { CategoryRouting } from './category-routing.module';
+import { AddCategoryComponent } from './components/add-category/add-category.component';
+import { UpdateCategoryComponent } from './components/update-category/update-category.component';
+import { DeleteCategoryComponent } from './components/delete-category/delete-category.component';
 
 
 
 @NgModule({
   declarations: [
-    ListCategoriesComponent
+    ListCategoriesComponent,
+    AddCategoryComponent,
+    UpdateCategoryComponent,
+    DeleteCategoryComponent,
   ],
   imports: [
     CommonModule,
